e367fb346a11e214ed517de561b9c442abee8677
Ajay <ajaykumar.kumar900@gmail.com>
2024-02-29 20:58:26 +0530

    initial commit

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
